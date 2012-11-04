1ea92d068975b83e722236d7abeb62e44dfea925
haxmjl <1332337+Spiridios@users.noreply.github.com>
2012-11-03 22:59:14 -0700

    Finish tileMap rendering prototype. Start refactoring into real tilemap code.

---
+++ SpiridiEngine/TileImage.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Xml.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Spiridios.SpiridiEngine
+{
+    class TileImage
+    {
+        private SpiridiGame game;
+        private Texture2D tileSet = null;
+        private int tileWidth = 0;
+        private int tileHeight = 0;
+
+        public int TileWidth
+        {
+            get { return tileWidth; }
+        }
+
+        public int TileHeight
+        {
+            get { return tileHeight; }
+        }
+
+        public TileImage(SpiridiGame game, XElement tileSetElement)
+        {
+            this.game = game;
+            LoadFromTiledElement(tileSetElement);
+        }
+        
+        private void LoadFromTiledElement(XElement tileSetElement)
+        {
+            XElement tileSetImage = tileSetElement.Element("image");
+            string tileSetImageSource = tileSetImage.Attribute("source").Value;
+            tileWidth = int.Parse(tileSetElement.Attribute("tilewidth").Value);
+            tileHeight = int.Parse(tileSetElement.Attribute("tileheight").Value);
+            tileSet = this.game.ImageManager.AddImage(tileSetImageSource, tileSetImageSource);
+            //tileSheetSize.X = int.Parse(tileSetImage.Attribute("width").Value);
+            //tileSheetSize.Y = int.Parse(tileSetImage.Attribute("height").Value);
+        }
+
+        // Move to Vector2Ext maybe
+        public static Vector2 GetImageCoordinatesFromOffset(int index, int imageWidth, int xMult, int yMult)
+        {
+            int y = index / imageWidth;
+            int x = index % imageWidth;
+            Vector2 coords = new Vector2(x * xMult, y * yMult);
+            return coords;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="spriteBatch"></param>
+        /// <param name="tileId">The ID of the tile to draw. 0 means don't draw the tile, 1 is the upper-leftmost tile, 2 is the tile to the right of that tile.</param>
+        public void DrawTile(SpriteBatch spriteBatch, int tileId, Rectangle destination)
+        {
+            if (tileId > 0)
+            {
+                Vector2 srcCoord = TileImage.GetImageCoordinatesFromOffset(tileId - 1, (tileSet.Width / tileWidth), tileWidth, tileHeight);
+                Rectangle source = new Rectangle((int)srcCoord.X, (int)srcCoord.Y, tileWidth, tileHeight);
+
+                spriteBatch.Draw(this.tileSet, destination, source, Color.White);
+            }
+
+        }
+
+    }
+}
--- SpiridiEngine/TileMap.cs
+++ SpiridiEngine/TileMap.cs
@@ -1,20 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using System.Xml.Linq;
 using System.IO;
 using System.IO.Compression;
+using System.Xml.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Spiridios.SpiridiEngine
 {
     public class TileMap : Drawable
     {
         private SpiridiGame game;
-        private Texture2D tileSet = null;
-        private List<int> mapGIDs = null;
+
+        // Need multiples of these.
+        private TileImage tileSet = null;
+
+        private List<int> layerTileIndices = null;
+        private Vector2 layerSize = new Vector2();
 
         public TileMap(SpiridiGame game, string tiledFile) 
         {
@@ -25,24 +27,20 @@ namespace Spiridios.SpiridiEngine
         private void LoadTiledMap(string tiledFile)
         {
             XDocument tiledDoc = XDocument.Load(tiledFile);
-            XElement tileSetElement = tiledDoc.Root.Element("tileset");
-            XElement tileSetImage = tileSetElement.Element("image");
-            string tileSetImageSource = tileSetImage.Attribute("source").Value;
-            tileSet = game.ImageManager.AddImage(tileSetImageSource, tileSetImageSource);
+            tileSet = new TileImage(game, tiledDoc.Root.Element("tileset"));
 
             XElement layerElement = tiledDoc.Root.Element("layer");
-            int layerHeight = int.Parse(layerElement.Attribute("height").Value);
-            int layerWidth = int.Parse(layerElement.Attribute("width").Value);
+            layerSize.Y = int.Parse(layerElement.Attribute("height").Value);
+            layerSize.X = int.Parse(layerElement.Attribute("width").Value);
             string layerString = layerElement.Element("data").Value;
             byte[] rawLayer = Convert.FromBase64String(layerString);
-            int size = layerWidth * layerHeight; 
-            mapGIDs = new List<int>(size);
-            using (BinaryReader stuff = new BinaryReader(new GZipStream(new MemoryStream(rawLayer), CompressionMode.Decompress)))
+            int size = (int)(layerSize.X * layerSize.Y); 
+            layerTileIndices = new List<int>(size);
+            using (BinaryReader layerReader = new BinaryReader(new GZipStream(new MemoryStream(rawLayer), CompressionMode.Decompress)))
             {
-
                 for (int i = 0; i < size; i++)
                 {
-                    mapGIDs.Add(stuff.ReadInt32());
+                    layerTileIndices.Add(layerReader.ReadInt32());
                 }
             }
             return;
@@ -50,7 +48,17 @@ namespace Spiridios.SpiridiEngine
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(this.tileSet, new Vector2(0, 0), Color.White);
+            int size = (int)(layerSize.X * layerSize.Y);
+            for (int i = 0; i < size; i++)
+            {
+                int gid = layerTileIndices[i];
+                if (gid > 0)
+                {
+                    Vector2 destCoord = TileImage.GetImageCoordinatesFromOffset(i, (int)layerSize.X, tileSet.TileWidth, tileSet.TileHeight);
+                    Rectangle dest = new Rectangle((int)destCoord.X, (int)destCoord.Y, tileSet.TileWidth, tileSet.TileHeight);
+                    tileSet.DrawTile(spriteBatch, gid, dest);
+                }
+            }
         }
 
     }
